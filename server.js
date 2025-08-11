8c07f3548b2678c856c31be34bf24632a4de65e0
Akshay Chaudhary <akshaypoonia00000@gmail.com>
2025-08-12 02:02:53 +0530

    All CRUD operation are done

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-
 import { port } from "./src/config/env.js";
 import app from "./src/app.js";
 import connectDB from "./src/config/db.js";
-
-console.log("server.js file running");
 
 connectDB();
 
 app.listen(port, () => {
 
-    console.log("listenning success");
+    console.log("listenning to server success");
     
 })
